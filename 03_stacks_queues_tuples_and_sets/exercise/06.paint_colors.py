ae59e4827732581d2101f61bfcdf31cfa2de5968
Ersin Nurtin <ersin.nurtin@outlook.com>
2023-01-17 11:53:59 +0200

    update

--- 03_stacks_queues_tuples_and_sets/exercise/06.paint_colors.py
+++ 03_stacks_queues_tuples_and_sets/exercise/06.paint_colors.py
@@ -1,62 +1,36 @@
-# from collections import deque
-#
-# color_substrings = deque(input().split())
-#
-# found_colors = []
-# found_secondary_colors = []
-#
-# primary_colors = ["red", "yellow", "blue"]
-# #
-# secondary_colors = {
-# 	"orange": ["red", "yellow"],
-# 	"purple": ["red", "blue"],
-# 	"green": ["yellow", "blue"],
-# }
-#
-# while len(color_substrings) > 1:
-#
-# 	first_substring = color_substrings.popleft()
-# 	last_substring = color_substrings.pop()
-#
-# 	if first_substring + last_substring in primary_colors:
-# 		found_colors.append(first_substring + last_substring)
-#
-# 	elif last_substring + first_substring in primary_colors:
-# 		found_colors.append(last_substring + first_substring)
-#
-# 	elif first_substring + last_substring in secondary_colors:
-# 		found_secondary_colors.append(first_substring + last_substring)
-# 		found_colors.append(first_substring + last_substring)
-#
-# 	elif last_substring + first_substring in secondary_colors:
-# 		found_secondary_colors.append(last_substring + first_substring)
-# 		found_colors.append(last_substring + first_substring)
-#
-# 	else:
-# 		index = len(color_substrings) // 2
-# 		if len(first_substring) > 1:
-# 			color_substrings.insert(index, first_substring[:-1])
-# 		if len(last_substring) > 1:
-# 			color_substrings.insert(index + 1, last_substring[:-1])
-#
-# last_substring = color_substrings.pop()
-#
-# if last_substring in primary_colors:
-# 	found_colors.append(last_substring)
-#
-# elif last_substring in secondary_colors:
-# 	found_secondary_colors.append(last_substring)
-# 	found_colors.append(last_substring)
-#
-# if secondary_colors:
-# 	keep_the_color = False
-# 	for color in found_secondary_colors:
-# 		for primary in secondary_colors[color]:
-# 			if primary in found_colors:
-# 				keep_the_color = True
-# 				continue
-# 			keep_the_color = False
-# 		if not keep_the_color:
-# 			found_colors.remove(color)
-#
-# print(found_colors)
+from collections import deque
+
+color_substrings = deque(input().split())
+
+found_colors = []
+
+colors = {
+	"all colors": ("red", "yellow", "blue", "orange", "purple", "green"),
+	"required_colors": {
+		"orange": ("red", "yellow"),
+		"purple": ("red", "blue"),
+		"green": ("yellow", "blue"),
+	},
+}
+
+while color_substrings:
+	last_substring = ""
+	if len(color_substrings) > 1:
+		last_substring = color_substrings.pop()
+	first_substring = color_substrings.popleft()
+
+	for color in (first_substring + last_substring, last_substring + first_substring):
+		if color in colors['all colors']:
+			found_colors.append(color)
+			break
+	else:
+		for item in (first_substring[:-1], last_substring[:-1]):
+			if item:
+				color_substrings.insert(len(color_substrings) // 2, item)
+				
+
+for color, required in colors["required_colors"].items():
+	if any(x not in found_colors and color in found_colors for x in required):
+		found_colors.remove(color)
+
+print(found_colors)
