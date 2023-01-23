cf9b77f0cde60737ccc6eb759aabf9702d079c25
Ersin Nurtin <ersin.nurtin@outlook.com>
2023-01-23 11:48:04 +0200

    some solutions

--- 03_multidimensional_lists/multidimensional_lists_exercise_2/05_alice_in_wonderland.py
+++ 03_multidimensional_lists/multidimensional_lists_exercise_2/05_alice_in_wonderland.py
@@ -1,43 +1,43 @@
-square_n = int(input())
-
-matrix, alice_pos = [], []
-tea_bags = 0
-
-directions = {
-	'up': (-1, 0),
-	'down': (1, 0),
-	'left': (0, -1),
-	'right': (0, 1)
-}
-
-for row in range(square_n):
-	matrix.append([int(x) if x.isdigit() else x for x in input().split()])
-	for col in range(square_n):
-		if matrix[row][col] == "A":
-			alice_pos = [row, col]
-			matrix[row][col] = '*'
-			
-
-while tea_bags < 10:
-	command = input()
-	
-	r, c = alice_pos[0] + directions[command][0], alice_pos[1] + directions[command][1]
-	if not (0 <= r < square_n and 0 <= c < square_n):
-		break
-	
-	element = matrix[r][c]
-	alice_pos = [r, c]
-	matrix[r][c] = '*'
-
-	if isinstance(element, int):
-		tea_bags += element
-		
-	elif element == "R":
-		break
-
-if tea_bags >= 10:
-	print("She did it! She went to the party.")
-else:
-	print("Alice didn't make it to the tea party.")
-	
-[print(' '.join([str(x) for x in row])) for row in matrix]
+# square_n = int(input())
+#
+# matrix, alice_pos = [], []
+# tea_bags = 0
+#
+# directions = {
+# 	'up': (-1, 0),
+# 	'down': (1, 0),
+# 	'left': (0, -1),
+# 	'right': (0, 1),
+# }
+#
+# for row in range(square_n):
+# 	matrix.append([int(x) if x.isdigit() else x for x in input().split()])
+# 	for col in range(square_n):
+# 		if matrix[row][col] == "A":
+# 			alice_pos = [row, col]
+# 			matrix[row][col] = '*'
+#
+#
+# while tea_bags < 10:
+# 	command = input()
+#
+# 	r, c = alice_pos[0] + directions[command][0], alice_pos[1] + directions[command][1]
+# 	if not (0 <= r < square_n and 0 <= c < square_n):
+# 		break
+#
+# 	element = matrix[r][c]
+# 	alice_pos = [r, c]
+# 	matrix[r][c] = '*'
+#
+# 	if isinstance(element, int):
+# 		tea_bags += element
+#
+# 	elif element == "R":
+# 		break
+#
+# if tea_bags >= 10:
+# 	print("She did it! She went to the party.")
+# else:
+# 	print("Alice didn't make it to the tea party.")
+#
+# [print(' '.join([str(x) for x in row])) for row in matrix]
---
+++ 03_multidimensional_lists/multidimensional_lists_exercise_2/06_range_day.py
@@ -0,0 +1,68 @@
+def valid_index(x, y):
+	if 0 <= x < 5 and 0 <= y < 5:
+		return True
+	return False
+
+
+def move_player(player_pos, data):
+	direction, steps = data[0], int(data[1])
+	
+	step = [x * steps if x != 0 else 0 for x in directions[direction]]
+	r, c = player_pos[0] + step[0], player_pos[1] + step[1]
+	
+	if valid_index(r, c):
+		if matrix[r][c] == '.':
+			player_pos = [r, c]
+	
+	return player_pos
+
+
+def shoot_target(player_pos, direction):
+	r, c = player_pos[0], player_pos[1]
+	
+	for _ in range(5):
+		r, c = r + directions[direction][0], c + directions[direction][1]
+		
+		if valid_index(r, c):
+			if matrix[r][c] == "x":
+				shot_targets.append([r, c])
+				matrix[r][c] = '.'
+				break
+		else:
+			break
+
+
+matrix, shooter_pos, shot_targets = [], [], []
+count_targets = 0
+
+directions = {
+	'up': (-1, 0),
+	'down': (1, 0),
+	'left': (0, -1),
+	'right': (0, 1),
+}
+
+for row in range(5):
+	matrix.append(input().split())
+	for col in range(5):
+		if matrix[row][col] == 'x':
+			count_targets += 1
+		elif matrix[row][col] == "A":
+			matrix[row][col] = '.'
+			shooter_pos = [row, col]
+
+for _ in range(int(input())):
+	command, *info = input().split()
+	
+	if command == "shoot":
+		shoot_target(shooter_pos, info[0])
+	
+	elif command == "move":
+		shooter_pos = move_player(shooter_pos, info)
+
+if len(shot_targets) == count_targets:
+	print(f"Training completed! All {count_targets} targets hit.")
+else:
+	print(f"Training not completed! {count_targets - len(shot_targets)} targets left.")
+	
+[print(target) for target in shot_targets]
