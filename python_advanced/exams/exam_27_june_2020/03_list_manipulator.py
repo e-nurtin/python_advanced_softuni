b7baa41df6cea48391f86bf22308bcb45ba106c2
Ersin Nurtin <ersin.nurtin@outlook.com>
2023-02-13 10:56:28 +0200

    oop

--- python_advanced/exams/exam_27_june_2020/03_list_manipulator.py
+++ python_advanced/exams/exam_27_june_2020/03_list_manipulator.py
@@ -1,29 +1,21 @@
 def add_to_list(list_to_add, info, *args):
-	result = []
 	if info == 'beginning':
-		result = list(args)
-		result.extend(list_to_add)
-	elif info == 'end':
-		result.extend(list_to_add)
-		result.extend(args)
+		[list_to_add.insert(0, num) for num in args]
 	
-	return result
+	elif info == 'end':
+		list_to_add.extend(args)
+	
+	return list_to_add
 
 
 def remove_from_list(list_of_numbers, info, *args):
+	to_remove = int(args[0]) if args else 1
+	
 	if info == 'beginning':
-		if args:
-			to_remove = int(args[0])
-			list_of_numbers = list_of_numbers[to_remove:]
-		else:
-			list_of_numbers = list_of_numbers[1:]
-			
+		list_of_numbers = list_of_numbers[to_remove:]
+	
 	elif info == 'end':
-		if args:
-			to_remove = int(args[0])
-			list_of_numbers = list_of_numbers[:-1 * to_remove]
-		else:
-			list_of_numbers.pop()
+		list_of_numbers = list_of_numbers[:-to_remove]
 	
 	return list_of_numbers
 
@@ -31,17 +23,9 @@ def remove_from_list(list_of_numbers, info, *args):
 def list_manipulator(list_of_numbers, action, info, *args):
 	if action == 'add':
 		list_of_numbers = add_to_list(list_of_numbers, info, *args)
+	
 	elif action == 'remove':
 		list_of_numbers = remove_from_list(list_of_numbers, info, *args)
 	
 	return list_of_numbers
 
-
-print(list_manipulator([1,2,3], "remove", "end"))
-print(list_manipulator([1,2,3], "remove", "beginning"))
-print(list_manipulator([1,2,3], "add", "beginning", 20))
-print(list_manipulator([1,2,3], "add", "end", 30))
-print(list_manipulator([1,2,3], "remove", "end", 2))
-print(list_manipulator([1,2,3], "remove", "beginning", 2))
-print(list_manipulator([1,2,3], "add", "beginning", 20, 30, 40))
-print(list_manipulator([1,2,3], "add", "end", 30, 40, 50))
